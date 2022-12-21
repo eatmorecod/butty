52cda2f3053696825833ad2485fb0b3b752abeff
eatmorecod <86938854+eatmorecod@users.noreply.github.com>
2022-12-21 05:23:34 -0500

    Update collab.py

--- collab.py
+++ collab.py
@@ -1,11 +1,18 @@
 import keyboard
 import serial
 import re
+from helpers_serial import position
 
 ser = serial.Serial('COM5', 38400, timeout=.1, parity='N')
-heading = False
-speed = False
-position = False
+
+def convertFromDD(dd):
+    min = (dd % 1) * 60
+    print(min)
+    ddmm = dd * 100 + min
+    print(ddmm) 
+
+is_gga = False
+is_hdt = False
 
 input("Press enter to spawn mothership.")
 
@@ -14,13 +21,67 @@ input("Press enter to spawn mothership.")
 # TODO Send Mothership GGA, HDT, VTG messages every .2 sec
 # TODO Update HDT, COG, and VTG on user input 
 
-while not heading or not position or not speed: 
+while not is_hdt or not is_gga: 
     line = ser.readline().decode('utf-8', errors='ignore')
     gga = re.search('GGA', line)
     hdt = re.search('HDT', line)
     hdg = re.search('HDG', line)
     vhw = re.search('VHW', line)
-    vtg = re.search('VTG', line)
+
+    if gga:
+        list = line.split(',')
+        lat = float(list[2])
+        print(lat)
+        deg = int(lat / 100)
+        min = lat % deg
+        lat = round(deg + (min / 60), 4)
+        print(lat)
+        
+        
+        lon = float(list[4])
+        deg = int(lon / 100)
+        min = lon % deg
+        lon = round(deg + (min / 60), 4)
+        if list[5] == "W":
+            lon *= -1
+
+        is_gga = True  
+
+    if hdt:
+        list = line.split(',')  
+        heading = float(list[1])
+        is_hdt = True
+
+    if hdg:
+        list = line.split(',')  
+        heading = float(list[1])
+        is_hdt = True
+
+    if vhw:
+        list = line.split(',')
+        heading = float(list[1])
+        is_hdt = True
+
+mothership = {
+    'lat': lat,
+    'lon': lon,
+    'hdg': heading,
+    'cog': heading,
+    'sog': 0.0,
+    'distance': 100  
+}
+
+
+print(mothership)
+print(convertFromDD(mothership['lat']))
+
+mothership = position(mothership)
+print(mothership)
+
+
+
+
+
 
 
 
