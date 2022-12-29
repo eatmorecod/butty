dfb3187776464163a6604fe4731b477d86d5f5f4
eatmorecod <86938854+eatmorecod@users.noreply.github.com>
2022-12-28 19:49:47 -0500

    Update collab2.py

--- collab2.py
+++ collab2.py
@@ -1,13 +1,32 @@
+import sys
 from pynput import keyboard
 import serial
 import re
-import sys
 from helpers_serial import position, make_checksum
+
+from math import radians, degrees
 from datetime import datetime
 from time import time
 
-SOG = 0
-HDG = 0
+
+print('\n\
+    *********************************************************************\n\
+    *********************************************************************\n\
+    ***  |||||||||   |||   |||   |||||||||   |||||||||   |||     |||  ***\n\
+    ***  |||    |||  |||   |||      |||         |||       |||   |||   ***\n\
+    ***  |||||||||   |||   |||      |||         |||         |||||     ***\n\
+    ***  |||    |||  |||   |||      |||         |||          |||      ***\n\
+    ***  |||||||||   |||||||||      |||         |||          |||      ***\n\
+    *********************************************************************\n\
+    *********************************************************************\n')
+mothership = {
+    'lat': 0.0,
+    'lon': 0.0,
+    'hdg': 0.0,
+    'cog': 0.0,
+    'sog': 0.0,
+    'distance': 100  
+}
 
 try:
     ser = serial.Serial('COM5', 38400, timeout=.1, parity='N')
@@ -20,32 +39,29 @@ def utcTime():
     timestamp = datetime.utcnow().strftime("%H%M%S.%f")[:-5]
     return timestamp
 
-
 def convertFromDD(dd):
     
+    dd = abs(dd)
     min = (dd % 1) * 60
-    print(min)
+    #print(min)
     ddmm = int(dd) * 100 + min
-    return ddmm
-    print(ddmm)
+    return round(ddmm, 4) 
 
 def on_press(key):
-    global HDG
-    global SOG
     try:
         match key:
             case keyboard.Key.right:
-                HDG += 1
-                if HDG >= 360: HDG -= 360
+                mothership['hdg'] += 1
+                if mothership['hdg'] >= 360: mothership['hdg'] -= 360
             case keyboard.Key.left:
-                HDG -= 1
-                if HDG < 0: HDG += 360
+                mothership['hdg'] -= 1
+                if mothership['hdg'] < 0: mothership['hdg'] += 360
             case keyboard.Key.up:
-                if SOG < 20:
-                    SOG +=1
+                if mothership['sog'] < 20:
+                    mothership['sog'] +=1
             case keyboard.Key.down:
-                if SOG > 0:
-                    SOG -=1
+                if mothership['sog'] > 0:
+                    mothership['sog'] -=1
     except:
         sys.exit() 
 
@@ -54,17 +70,12 @@ def on_release(key):
         # Stop listener
         return False
 
-
-is_gga = True
-is_hdt = True
+is_gga = False
+is_hdt = False
 
 input("Press enter to spawn mothership.")
 
-# TODO Get Ownship GGA from ser
-# TODO Define Mothership Dict with ownship position, and 0 COG, HDT, SOG
-# TODO Send Mothership GGA, HDT, VTG messages every .2 sec
-# TODO Update HDT, COG, and VTG on user input 
-
+# Get Ownship GGA from ser
 if ser:
     while not is_hdt or not is_gga: 
         line = ser.readline().decode('utf-8', errors='ignore')
@@ -80,64 +91,72 @@ if ser:
             deg = int(lat / 100)
             min = lat % deg
             lat = round(deg + (min / 60), 4)
-            print(lat)
+            mothership['lat'] = radians(lat)
+            #print(lat)
             
             
             lon = float(list[4])
             deg = int(lon / 100)
             min = lon % deg
             lon = round(deg + (min / 60), 4)
+            #print(lon)
             if list[5] == "W":
                 lon *= -1
+            mothership['lon'] = radians(lon)
+            
 
             is_gga = True  
 
         if hdt:
             list = line.split(',')  
-            heading = float(list[1])
+            mothership['hdg'] = float(list[1])
             is_hdt = True
 
         if hdg:
             list = line.split(',')  
-            heading = float(list[1])
+            mothership['hdg'] = float(list[1]) - float(list[4])
             is_hdt = True
 
-        if vhw:
+        '''if vhw:
             list = line.split(',')
             heading = float(list[1])
-            is_hdt = True
+            is_hdt = True'''
 
-    mothership = {
-        'lat': lat,
-        'lon': lon,
-        'hdg': int(heading),
-        'cog': 'hdg',
-        'sog': 0,
-        'distance': 100  
-    }
+    ser.close()
 
-'''print(mothership)
-print(convertFromDD(mothership['lat']))
+    #print(mothership)
+    #print(degrees(mothership['lat']))
+    #print(degrees(mothership['lon']))
+    #print(convertFromDD(mothership['lat']))
 
-mothership = position(mothership)
-print(mothership)'''
+    #mothership = position(mothership)
+    #print(degrees(mothership['lat']))
+    #print(degrees(mothership['lon']))
 
-mothership = {
-        'lat': 4221.7866,
-        'lon': 07101.8926,
-        'hdg': 1,
-        'cog': 'hdg',
-        'sog': 0,
-        'distance': 100  
-    }
 def getGGA():
-    lat = convertFromDD(mothership['lat'])
-    lon = convertFromDD(mothership['lon'])
-    msg = f'$GPGGA,184854.3,4221.7866,N,07101.8926,W,2,28,0.61,-24.72,M,-33.13,M,,*'
-    #msg = f'$GPGGA,{utcTime()},{lat},N,{lon},W,2,28,0.61,-24.53,M,-33.13,M,,*'
+    lat = convertFromDD(degrees(mothership['lat']))
+    lon = convertFromDD(degrees(mothership['lon']))
+    msg = f'$GPGGA,{utcTime()},{lat},N,{lon},W,2,28,0.61,-24.72,M,-33.13,M,,*'
+    return msg + f'{make_checksum(msg):02x}'
+
+def getHDG():
+    msg = f'$HCHDG,{mothership["hdg"]}.0,,,,*'
+    return msg + f'{make_checksum(msg):02x}'
+
+def getVTG():
+    msg = f'$GPVTG,{mothership["cog"]}.0,T,,M,{round(mothership["sog"])}.0,N,{round(mothership["sog"] * 1.852, 2)},K,D*'
     return msg + f'{make_checksum(msg):02x}'
 
 print(getGGA())
+print(getHDG())
+print(getVTG())
+
+# Open serial port to transmit mothership GPS
+try:
+    serTx = serial.Serial('COM6', 38400, timeout=.1, parity='N')
+except:
+    serTx = None
+    print('COM6 not found')
 
 # Start non-blocking keyboard listener to update heading and speed
 listener = keyboard.Listener(
@@ -146,7 +165,20 @@ listener = keyboard.Listener(
 
 listener.start()
 timeOfLastMsg = time()
+
 while listener.running:
+    # Send Mothership GGA, HDT, VTG messages every .2 sec
     if timeOfLastMsg + .2 <= time():
-        sys.stdout.write(f'\rHEADING: {HDG:0=3d}°\tSPEED(kn): {SOG:<2d}\tTIME: {utcTime()}')  
+        mothership['distance'] = (mothership['sog'] / 1.944) * (time() - timeOfLastMsg)
+        mothership['cog'] = mothership['hdg']
+        mothership = position(mothership)
+        serTx.write(getGGA().encode('utf-8', errors='ignore'))
+        serTx.write(getHDG().encode('utf-8', errors='ignore'))
+        serTx.write(getVTG().encode('utf-8', errors='ignore'))
+        #sys.stdout.write(f'\rHEADING: {int(mothership["hdg"]):0=3d}°\tSPEED(kn): {int(mothership["sog"]):<2d}\tTIME: {utcTime()}')  
         timeOfLastMsg = time()
+        print(getGGA())
+        print(getHDG())
+        print(getVTG())
+
+serTx.close()
